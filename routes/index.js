5b20374919603612a86da4b11f157d950e62ab2f
alweky <alweky@yahoo.ca>
2015-03-22 18:52:48 -0400

    Some image commits

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('index2', { title: 'About Me' });
 });
 
 /* GET projects page. */
