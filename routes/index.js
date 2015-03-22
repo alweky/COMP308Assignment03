e796f6ef64279444e93b3f61d5b2e7c01b4b9ec7
alweky <alweky@yahoo.ca>
2015-03-22 19:07:56 -0400

    Added About me

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('index2', { title: 'About Me' });
+router.get('/aboutMe', function(req, res, next) {
+  res.render('aboutMe', { title: 'About Me' });
 });
 
 /* GET projects page. */
