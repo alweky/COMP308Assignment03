7dd7d9a9bb0576ce71028643c95fc95dc766c5fb
alweky <alweky@yahoo.ca>
2015-03-22 19:18:49 -0400

    project Page

--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,17 @@ router.get('/aboutMe', function(req, res, next) {
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contactMe', { title: 'Contact Me' });
 });
 
 module.exports = router;
