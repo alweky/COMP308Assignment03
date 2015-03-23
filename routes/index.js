096f29202384be6eb0ccb21975db3477080428e8
alweky <alweky@yahoo.ca>
2015-03-22 22:30:35 -0400

    Added active stuff...

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome Page' });
+  res.render('index', { active1: 'active', active2: '', active3: 'has-dropdown' });
 });
 
 /* GET about page. */
 router.get('/aboutMe', function(req, res, next) {
-  res.render('aboutMe', { title: 'About Me' });
+  res.render('aboutMe', { active2: 'active', active1: '', active3: '' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { active3: 'has-dropdown active', active1: '', active2: '' });
+});
+
+/* GET sub projects page. */
+router.get('/projects/projectAdvancedSpaceWars', function(req, res, next) {
+  res.render('projects/projectAdvancedSpaceWars', { title: 'Projects' });
+});
+
+router.get('/projects/projectAndroidHealth', function(req, res, next) {
+  res.render('projects/projectAndroidHealth', { title: 'Projects' });
+});
+
+router.get('/projects/projectJumper', function(req, res, next) {
+  res.render('projects/projectJumper', { title: 'Projects' });
+});
+
+router.get('/projects/projectSolarSystem', function(req, res, next) {
+  res.render('projects/projectSolarSystem', { title: 'Projects' });
 });
 
 /* GET services page. */
